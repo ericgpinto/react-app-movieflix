7ac2d4c4328b0c126ce25acc9c8a7b1d4da1f7bf
ericgpinto <ericgrandopinto@gmail.com>
2024-07-29 13:08:38 -0300

    feat(api): consuming movies api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 export default function Home() {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "8b1a17fced64b51993df10a7d96efdc4",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      console.log(response.data.results);
+    }
+
+    loadMovies();
+  }, []);
+
   return (
     <div>
       <h1>Welcome to Home</h1>
